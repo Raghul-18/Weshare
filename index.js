6fac9da2a950659c6ce1a623194fb0e2242a5606
Raghul-18 <87120429+Raghul-18@users.noreply.github.com>
2021-07-15 21:14:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://we-share-file-transfer.herokuapp.com/";
-const uploadURL = `${baseURL}/api/files`;
+const baseURL = "https://we-share-file-transfer.herokuapp.com";
+const uploadURL = `${baseURL}/files/api`;
 const emailURL = `${baseURL}/api/files/send`;
 
 const maxAllowedSize = 100 * 1024 * 1024; //100mb
